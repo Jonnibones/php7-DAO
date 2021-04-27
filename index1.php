879c9b410eb5672167dbd7c7ba88d946be203d94
Jonnibones <js5800672@gmail.com>
2021-04-27 19:59:42 -0300

    Criada a classe usuario

--- index1.php
+++ index1.php
@@ -2,11 +2,21 @@
 
 require_once("config.php");
 
-$sql = new Sql();
+
+
+$root = new Usuario();
+
+$root->loadByid(1);
+
+echo $root;
+
+
+
+/*$sql = new Sql();
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);
-
+*/
 
  ?>

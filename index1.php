48c8bfd6543002bca99db39f190f83aa8218772c
Jonnibones <js5800672@gmail.com>
2021-04-27 22:08:23 -0300

    feito os metodos de lista

--- index1.php
+++ index1.php
@@ -3,20 +3,22 @@
 require_once("config.php");
 
 
-
-$root = new Usuario();
-
+//Listar 1 usuário
+/*$root = new Usuario();
 $root->loadByid(1);
+echo $root;*/
 
-echo $root;
+//carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
+//carrega usuarios por iniciais
+//$search = Usuario::search("jo");
+//echo json_encode($search);
 
-
-/*$sql = new Sql();
-
-$usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
-echo json_encode($usuarios);
-*/
+//carrega usuario usando login e senha
+$usuario = new Usuario();
+$usuario->login("Jonathan", "1234");
+echo $usuario;
 
  ?>
